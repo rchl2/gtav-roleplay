d77c54193839b7f88adfb3778966f1414dcbd35c
rchl2 <mat.zarnowskii+git@gmail.com>
2018-10-10 08:32:22 +0200

    Add missing ownerType column inside vehicles model.

--- migrations/20180609121607-create-vehicles.js
+++ migrations/20180609121607-create-vehicles.js
@@ -32,6 +32,9 @@ module.exports = {
       owner: {
         type: Sequelize.INTEGER
       },
+      ownerType: {
+        type: Sequelize.INTEGER
+      },
       primaryColor: {
         type: Sequelize.STRING
       },
