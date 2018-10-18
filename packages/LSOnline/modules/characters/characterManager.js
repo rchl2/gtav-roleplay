9d1d4fde77955f7c2d9bb606923a84b55149fc02
kubeeek <jakub.galecki5@gmail.com>
2018-10-18 16:44:33 +0200

    Saving time played, bank, money and health at player exit

--- packages/LSOnline/modules/characters/characterManager.js
+++ packages/LSOnline/modules/characters/characterManager.js
@@ -2,7 +2,6 @@
 
 const logger = require('../utils/logger');
 const database = require('../database/database');
-
 exports.loadAndSpawnCharacter = async (player, characterId) => {
   const character = await database.character.findById(characterId).then(character => {
     return character.dataValues;
@@ -18,14 +17,18 @@ exports.findCharactersForAccount = async (accountId) => {
   }).then(characters => characters);
 };
 
-exports.saveCharacterBeforeQuit = async (characterId, dimension, position, lastVehicle = false, lastLogin, exitType) => {
+exports.saveCharacterBeforeQuit = async (characterId, dimension, position, lastVehicle = false, lastLogin, exitType, health, played, money, bank) => {
   database.character.findById(characterId).then(character => {
     character
       .update({
         position: JSON.stringify(position),
         dimension: dimension,
         lastLogin: lastLogin,
-        lastExitType: exitType
+        lastExitType: exitType,
+        played,
+        bank,
+        money,
+        health
       })
       .then((character) => {
         logger('character', `Saved character "${character.name}" (ID: ${character.id} / Owner: ${character.owner}) data before quit.`, 'info');
